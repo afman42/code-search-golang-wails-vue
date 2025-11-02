5f656a6699625ba19a2d86ff3a3e2f9e2a9c852a
afman42 <afman42@afman42.afman42>
2025-11-02 21:08:34 +0700

    feat: refactor vue is not defined

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,30 +1,28 @@
 export default {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  moduleFileExtensions: ['vue', 'ts', 'js', 'json'],
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"],
+  },
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  moduleFileExtensions: ["vue", "ts", "js", "json"],
   transform: {
-    '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.ts$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
+    "^.+\\.vue$": "@vue/vue3-jest",
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^../../wailsjs/go/main/App$': '<rootDir>/tests/__mocks__/wailsAppMock.js',
-    '^../../wailsjs/runtime/runtime$': '<rootDir>/tests/__mocks__/wailsRuntimeMock.js',
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^../../wailsjs/go/main/App$": "<rootDir>/tests/__mocks__/wailsAppMock.js",
+    "^../../wailsjs/runtime/runtime$":
+      "<rootDir>/tests/__mocks__/wailsRuntimeMock.js",
   },
-  transformIgnorePatterns: [
-    '/node_modules/(?!@vue)',
-  ],
-  testMatch: ['**/tests/**/*.spec.(ts|js)?(x)'],
+  transformIgnorePatterns: ["/node_modules/(?!@vue)"],
+  testMatch: ["**/tests/**/*.spec.(ts|js)?(x)"],
   collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.{ts,vue}',
-    '!src/main.ts',
-    '!src/**/types/*',
-  ],
+  collectCoverageFrom: ["src/**/*.{ts,vue}", "!src/main.ts", "!src/**/types/*"],
   globals: {
-    'ts-jest': {
+    "ts-jest": {
       useESM: true,
     },
   },
