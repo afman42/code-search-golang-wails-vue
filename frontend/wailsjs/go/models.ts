2016a392cd9c1b6a909fbe294fe3fc6f88b77d5b
afman42 <afman42@afman42.afman42>
2025-11-02 20:12:11 +0700

    feat: generated from wails

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,59 +1,5 @@
 export namespace main {
 	
-	export class SearchResult {
-	    filePath: string;
-	    lineNum: number;
-	    content: string;
-	    matchedText: string;
-	    contextBefore: string[];
-	    contextAfter: string[];
-	
-	    static createFrom(source: any = {}) {
-	        return new SearchResult(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.filePath = source["filePath"];
-	        this.lineNum = source["lineNum"];
-	        this.content = source["content"];
-	        this.matchedText = source["matchedText"];
-	        this.contextBefore = source["contextBefore"];
-	        this.contextAfter = source["contextAfter"];
-	    }
-	}
-	export class ExportRequest {
-	    results: SearchResult[];
-	    format: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new ExportRequest(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.results = this.convertValues(source["results"], SearchResult);
-	        this.format = source["format"];
-	    }
-	
-		convertValues(a: any, classs: any, asMap: boolean = false): any {
-		    if (!a) {
-		        return a;
-		    }
-		    if (a.slice && a.map) {
-		        return (a as any[]).map(elem => this.convertValues(elem, classs));
-		    } else if ("object" === typeof a) {
-		        if (asMap) {
-		            for (const key of Object.keys(a)) {
-		                a[key] = new classs(a[key]);
-		            }
-		            return a;
-		        }
-		        return new classs(a);
-		    }
-		    return a;
-		}
-	}
 	export class SearchRequest {
 	    directory: string;
 	    query: string;
@@ -84,6 +30,28 @@ export namespace main {
 	        this.searchSubdirs = source["searchSubdirs"];
 	        this.useRegex = source["useRegex"];
 	        this.excludePatterns = source["excludePatterns"];
+	    }
+	}
+	export class SearchResult {
+	    filePath: string;
+	    lineNum: number;
+	    content: string;
+	    matchedText: string;
+	    contextBefore: string[];
+	    contextAfter: string[];
+	
+	    static createFrom(source: any = {}) {
+	        return new SearchResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.filePath = source["filePath"];
+	        this.lineNum = source["lineNum"];
+	        this.content = source["content"];
+	        this.matchedText = source["matchedText"];
+	        this.contextBefore = source["contextBefore"];
+	        this.contextAfter = source["contextAfter"];
 	    }
 	}
 
